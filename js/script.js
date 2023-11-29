2e0f21fd72f45db493574e16bd947efc20e43e0e
P P SENEVIRATHNA <pasiya20024@gmail.com>
2023-11-29 13:23:30 +0530

    add scroll top butoon

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 (function() {
   
-  /**
-   * Initiate Pure Counter 
-   */
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function() {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    }));
+  }
   new PureCounter();
 
 })()
